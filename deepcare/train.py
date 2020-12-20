3699a0dd840954b971373aaa1151177a6f4d4b36
Mario Nowak <mnowak@students.uni-mainz.de>
2020-12-20 12:05:14 +0100

    training saves model and prints out learning time

--- deepcare/train.py
+++ deepcare/train.py
@@ -1,4 +1,7 @@
 from dataset import MSADataset
+from models.conv_net import ConvNetv1
+
+import time
 
 import torch
 import torch.nn as nn
@@ -6,6 +9,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
+import os
 
 
 class Net(nn.Module):
@@ -64,10 +68,10 @@ if __name__ == "__main__":
 
     device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-    num_epochs = 100 
+    num_epochs = 50 
     learning_rate = 0.00001
     train_CNN = False
-    batch_size = 32
+    batch_size = 128
     shuffle = True
     pin_memory = True
     num_workers = 1
@@ -80,29 +84,31 @@ if __name__ == "__main__":
         )
 
     dataset = MSADataset(
-        root_dir="datasets/center_base_dataset_11_100_human_readable",
-        annotation_file="datasets/center_base_dataset_11_100_human_readable/train_labels.csv",
+        root_dir="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_human_readable",
+        annotation_file="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_human_readable/train_labels.csv",
         transform=transform
         )
 
-    print(dataset.__len__())
-
-    train_set, validation_set = torch.utils.data.random_split(dataset,[20000,4000])
+    train_set, validation_set = torch.utils.data.random_split(dataset,[52000,12000])
     train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
     validation_loader = DataLoader(dataset=validation_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers, pin_memory=pin_memory)
 
     # Model
-    model = Net()
+    model = ConvNetv1()
     model.to(device)
 
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss() 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    start_time = time.time()
+    print("Starting the training process.")
+
     # Train Network
     for epoch in range(num_epochs):
         losses = []
         
+        epoch_start_time = time.time()
         for batch_idx, (data, targets) in enumerate(train_loader):
             # Get data to cuda if possible
             data = data.to(device=device)
@@ -120,12 +126,27 @@ if __name__ == "__main__":
             
             # gradient descent or adam step
             optimizer.step()
-
-        print(f'Cost at epoch {epoch} is {sum(losses)/len(losses)}')
         
+        epoch_end_time = time.time()
+        epoch_duration = epoch_end_time - epoch_start_time
+        print(f'Cost at epoch {epoch} is {sum(losses)/len(losses)}. Training the epoch took {epoch_duration} seconds.')
+
+        if epoch+1 % 10 == 0:
+            print(f"Accuracy on the validation set is {check_accuracy(validation_loader, model)}")
+    
+    end_time = time.time()
+    duration = end_time - start_time
+
+    print(f"Finished training. The process took {duration} seconds.")    
+    
     print("Checking accuracy on Training Set")
     check_accuracy(train_loader, model)
 
     print("Checking accuracy on Test Set")
     check_accuracy(validation_loader, model)
 
+    save_path = "trained_models"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    torch.save(model.state_dict(), os.path.join(save_path, "conv_net_humanchr1430_center_base_w51_h100_n64000"))
