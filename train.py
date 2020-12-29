c94c55133166574da3440c7e35cf386f709e6537
Mario Nowak <mnowak@students.uni-mainz.de>
2020-12-29 11:01:01 +0100

    adjust import statements

--- train.py
+++ train.py
@@ -1,98 +1,99 @@
-from deepcare.dataset import MSADataset
-from deepcare.models.conv_net import ConvNetv1
-
+import os
 import time
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
-import os
+from torch.utils.data import DataLoader
 
+from deepcare.data import MSADataset
+from deepcare.models.conv_net import conv_net_w51_h100
+from deepcare.utils.accuracy import check_accuracy
 
-device = ("cuda" if torch.cuda.is_available() else "cpu")
+if __name__ == "__main__":
 
-num_epochs = 100
-learning_rate = 0.00001
-batch_size = 256
-shuffle = True
-pin_memory = True
-num_workers = 1
+    device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5)),
-        ]
-    )
+    num_epochs = 100
+    learning_rate = 0.00001
+    batch_size = 256
+    shuffle = True
+    pin_memory = True
+    num_workers = 1
 
-dataset = MSADataset(
-    root_dir="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_not_human_readable",
-    annotation_file="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_not_human_readable/train_labels.csv",
-    transform=transform
-    )
+    transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5)),
+            ]
+        )
 
-train_set, validation_set = torch.utils.data.random_split(dataset,[52000,12000])
-train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
-validation_loader = DataLoader(dataset=validation_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers, pin_memory=pin_memory)
+    dataset = MSADataset(
+        root_dir="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_not_human_readable",
+        annotation_file="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_not_human_readable/train_labels.csv",
+        transform=transform
+        )
 
-# Model
-model = Net()
-model.to(device)
+    train_set, validation_set = torch.utils.data.random_split(dataset,[52000,12000])
+    train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
+    validation_loader = DataLoader(dataset=validation_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers, pin_memory=pin_memory)
 
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss() 
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # Model
+    model = conv_net_w51_h100()
+    model.to(device)
 
-start_time = time.time()
-print("Starting the training process.")
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss() 
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# Train Network
-for epoch in range(num_epochs):
-    losses = []
-    
-    epoch_start_time = time.time()
-    for batch_idx, (data, targets) in enumerate(train_loader):
-        # Get data to cuda if possible
-        data = data.to(device=device)
-        targets = targets.to(device=device)
+    start_time = time.time()
+    print("Starting the training process.")
+
+    # Train Network
+    for epoch in range(num_epochs):
+        losses = []
         
-        # forward
-        scores = model(data)
-        loss = criterion(scores, targets)
+        epoch_start_time = time.time()
+        for batch_idx, (data, targets) in enumerate(train_loader):
+            # Get data to cuda if possible
+            data = data.to(device=device)
+            targets = targets.to(device=device)
+            
+            # forward
+            scores = model(data)
+            loss = criterion(scores, targets)
+            
+            losses.append(loss.item())
+            
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+            
+            # gradient descent or adam step
+            optimizer.step()
         
-        losses.append(loss.item())
-        
-        # backward
-        optimizer.zero_grad()
-        loss.backward()
-        
-        # gradient descent or adam step
-        optimizer.step()
-    
-    epoch_end_time = time.time()
-    epoch_duration = epoch_end_time - epoch_start_time
-    print(f'Cost at epoch {epoch} is {sum(losses)/len(losses)}. Training the epoch took {epoch_duration} seconds.')
+        epoch_end_time = time.time()
+        epoch_duration = epoch_end_time - epoch_start_time
+        print(f'Cost at epoch {epoch} is {sum(losses)/len(losses)}. Training the epoch took {epoch_duration} seconds.')
 
-    if (epoch+1) % 10 == 0:
-        print(f"Accuracy on the validation set after epoch {epoch}:") 
-        check_accuracy(validation_loader, model)
+        if (epoch+1) % 10 == 0:
+            print(f"Accuracy on the validation set after epoch {epoch}:") 
+            check_accuracy(validation_loader, model)
 
-end_time = time.time()
-duration = end_time - start_time
+    end_time = time.time()
+    duration = end_time - start_time
 
-print(f"Finished training. The process took {duration} seconds.")    
+    print(f"Finished training. The process took {duration} seconds.")    
 
-print("Checking accuracy on Training Set")
-check_accuracy(train_loader, model)
+    print("Checking accuracy on Training Set")
+    check_accuracy(train_loader, model)
 
-print("Checking accuracy on Test Set")
-check_accuracy(validation_loader, model)
+    print("Checking accuracy on Test Set")
+    check_accuracy(validation_loader, model)
 
-save_path = "trained_models"
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
+    save_path = "trained_models"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
 
-torch.save(model.state_dict(), os.path.join(save_path, "simple_conv_net_humanchr1430_center_base_w51_h100_n64000_not_human_readable"))
+    torch.save(model.state_dict(), os.path.join(save_path, "simple_conv_net_humanchr1430_center_base_w51_h100_n64000_not_human_readable"))
