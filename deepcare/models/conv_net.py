d8081588fc2474d8537e37009f5c8ea5f6f1c502
Mario Nowak <mnowak@students.uni-mainz.de>
2020-12-29 14:18:12 +0100

    new convnet

--- deepcare/models/conv_net.py
+++ deepcare/models/conv_net.py
@@ -78,3 +78,38 @@ class ConvNetW51H100V1(nn.Module):
 
 def conv_net_w51_h100():
     return ConvNetW51H100V1()
+
+
+class ConvNetW51H100V2(nn.Module):
+
+    def __init__(self):
+        super(ConvNetW51H100V2, self).__init__()
+        # 4 input image channel, 6 output channels, 3x3 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(4, 6, (5,3))
+        self.conv2 = nn.Conv2d(6, 16, (7,5))
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 21 * 10, 120)  # 6*6 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 4)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+def conv_net_w51_h100_v2():
+    return ConvNetW51H100V2()
