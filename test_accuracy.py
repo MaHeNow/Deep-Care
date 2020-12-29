4547e58c1ada41e1016762e0ae1558adf048aa49
Mario Nowak <mnowak@students.uni-mainz.de>
2020-12-29 10:58:03 +0100

    adjust import statements

--- test_accuracy.py
+++ test_accuracy.py
@@ -1,18 +1,22 @@
 import os
+
 import torch
-from deepcare.train import check_accuracy_on_classes, Net
-from deepcare.dataset import MSADataset
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
-model_name = "simple_conv_net_humanchr1430_center_base_w51_h100_n64000_not_human_readable"
-model_path = "trained_models"
+from deepcare.utils.accuracy import check_accuracy_on_classes
+from deepcare.data import MSADataset
+from deepcare.models.conv_net import conv_net_w51_h100
+
 
 if __name__ == "__main__":
 
     device = ("cuda" if torch.cuda.is_available() else "cpu")
+    
+    model_path = "trained_models"
+    model_name = "simple_conv_net_humanchr1430_center_base_w51_h100_n64000_not_human_readable"
 
-    shuffle = False
+    shuffle = True
     batch_size = 256
     pin_memory = True
     num_workers = 1
@@ -32,7 +36,7 @@ if __name__ == "__main__":
 
     loader = DataLoader(dataset=dataset, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
     
-    model = Net()
+    model = conv_net_w51_h100()
     model.load_state_dict(torch.load(os.path.join(model_path, model_name)))
     model.to(device)
 
