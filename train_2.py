2630d60ec4431805fd7d07f03d299173ab53f5e1
Mario Nowak <mnowak@students.uni-mainz.de>
2020-12-31 16:46:50 +0100

    training different models

--- train_2.py
+++ train_2.py
@@ -8,7 +8,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 from deepcare.data import MSADataset
-from deepcare.models.conv_net import conv_net_w51_h100, conv_net_w51_h100_v3, conv_net_w51_h100_v4
+from deepcare.models.conv_net import conv_net_w51_h100, conv_net_w51_h100_v3, conv_net_w51_h100_v4, conv_net_w250_h50_v1
 from deepcare.utils.accuracy import check_accuracy, check_accuracy_on_classes
 
 if __name__ == "__main__":
@@ -22,11 +22,11 @@ if __name__ == "__main__":
     pin_memory = True
     num_workers = 1
     dataset_folder = "datasets"
-    dataset_name = "humanchr1430covMSA_center_base_dataset_w51_h100_n64000_human_readable"
+    dataset_name = "humanchr1430covMSA_part_7_9_center_base_dataset_w250_h50_n64000_human_readable"
     validationset_name = "humanchr1430covMSA_part5_center_base_dataset_w51_h100_n64000_human_readable"
     dataset_csv_file = "train_labels.csv"
     model_out_dir = "trained_models"
-    model_name = "simple_conv_net_v4_humanchr1430_center_base_w51_h100_n64000_human_readable"
+    model_name = "conv_net_v1_humanchr1430_center_base_w250_h150_n64000_human_readable"
 
     transform = transforms.Compose(
             [
@@ -35,19 +35,19 @@ if __name__ == "__main__":
             ]
         )
 
-    train_set = MSADataset(
+    dataset = MSADataset(
         root_dir=os.path.join(dataset_folder, dataset_name),
         annotation_file=os.path.join(dataset_folder, dataset_name, dataset_csv_file),
         transform=transform
         )
     
-    validation_set = MSADataset(
-        root_dir=os.path.join(dataset_folder, validationset_name),
-        annotation_file=os.path.join(dataset_folder, validationset_name, dataset_csv_file),
-        transform=transform
-    )
+    #validation_set = MSADataset(
+    #    root_dir=os.path.join(dataset_folder, validationset_name),
+    #    annotation_file=os.path.join(dataset_folder, validationset_name, dataset_csv_file),
+    #    transform=transform
+    #)
 
-    #train_set, validation_set = torch.utils.data.random_split(dataset,[52000,12000])
+    train_set, validation_set = torch.utils.data.random_split(dataset,[52000,12000])
     train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
     validation_loader = DataLoader(dataset=validation_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers, pin_memory=pin_memory)
 
