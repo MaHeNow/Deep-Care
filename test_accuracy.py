fe5b71e9ba5d04bdfe5c0ba723bfbc08049712b3
Mario Nowak <mnowak@students.uni-mainz.de>
2020-12-31 16:48:21 +0100

    minor changes

--- test_accuracy.py
+++ test_accuracy.py
@@ -6,17 +6,21 @@ import torchvision.transforms as transforms
 
 from deepcare.utils.accuracy import check_accuracy_on_classes
 from deepcare.data import MSADataset
-from deepcare.models.conv_net import conv_net_w51_h100
+from deepcare.models.conv_net import conv_net_w51_h100, conv_net_w51_h100_v2, conv_net_w51_h100_v3, conv_net_w51_h100_v4
 
 
 if __name__ == "__main__":
 
     device = ("cuda" if torch.cuda.is_available() else "cpu")
     
-    model_path = "trained_models"
-    model_name = "simple_conv_net_humanchr1430_center_base_w51_h100_n64000_not_human_readable"
+    dataset_folder = "datasets"
+    dataset_name = "humanchr1430covMSA_part5_center_base_dataset_w51_h100_n64000_human_readable"
 
-    shuffle = True
+    model_path = "trained_models"
+    model_name = "simple_conv_net_v4_humanchr1430_center_base_w51_h100_n64000_human_readable"
+    model = conv_net_w51_h100_v4()
+
+    shuffle = False
     batch_size = 256
     pin_memory = True
     num_workers = 1
@@ -29,14 +33,13 @@ if __name__ == "__main__":
         )
 
     dataset = MSADataset(
-        root_dir="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_not_human_readable",
-        annotation_file="datasets/humanchr1430covMSA_center_base_dataset_w51_h100_n64000_not_human_readable/train_labels.csv",
+        root_dir=os.path.join(dataset_folder, dataset_name),
+        annotation_file=os.path.join(dataset_folder, dataset_name, "train_labels.csv"),
         transform=transform
         )
 
     loader = DataLoader(dataset=dataset, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
     
-    model = conv_net_w51_h100()
     model.load_state_dict(torch.load(os.path.join(model_path, model_name)))
     model.to(device)
 
