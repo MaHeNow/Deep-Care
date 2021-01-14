f2480479b3b670bae86713c829d59ef1d252a42f
Mario Nowak <mnowak@students.uni-mainz.de>
2021-01-14 16:30:51 +0100

    improved training script

--- train.py
+++ train.py
@@ -6,80 +6,135 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import pandas as pd
 
 from deepcare.data import MSADataset
-from deepcare.models.conv_net import conv_net_w51_h100_v1, conv_net_w51_h100_v3, conv_net_w51_h100_v4, conv_net_w250_h50_v1, conv_net_w250_h50_v3
+from deepcare.models.conv_net import conv_net_w51_h100_v1, conv_net_w51_h100_v3, conv_net_w51_h100_v4, conv_net_w51_h100_v5,conv_net_w250_h50_v1, conv_net_w250_h50_v3
 from deepcare.utils.accuracy import check_accuracy, check_accuracy_on_classes
+
 
 if __name__ == "__main__":
 
+    # Check if training on the GPU is possible
     device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-    num_epochs = 180
+
+    # -------------- Hyperparameters -------------------------------------------
+    num_epochs = 10
     learning_rate = 0.00001
     batch_size = 256
     shuffle = True
     pin_memory = True
     num_workers = 20
+
+
+    # -------------- File structure --------------------------------------------
     dataset_folder = "datasets"
     dataset_name = "humanchr1430covMSA_non_hq_part4_center_base_dataset_w51_h100_n279295_not_human_readable"
-    #validationset_name = "humanchr1430covMSA_part1_center_base_dataset_w51_h100_n24000_human_readable"
+    validationset_name = "" #"humanchr1430covMSA_part1_center_base_dataset_w51_h100_n24000_human_readable"
+    validation_split = 0.25
     dataset_csv_file = "train_labels.csv"
-    model_out_dir = "trained_models"
-    model_name = "conv_net_v4_humanchr1430_center_base_w51_h100_n384000_not_human_readable_part4_non_hq"
+    
+    model_out_dir = "trained_models/conv_net_v4_humanchr1430_center_base_w51_h100_n384000_not_human_readable"
+    model_name = "conv_net_v4_state_dict"
 
-    # Model
+
+    # -------------- Preparing the Model ---------------------------------------
     model = conv_net_w51_h100_v4()
     model.to(device)
 
-    transform = transforms.Compose(
-            [
+
+    # -------------- Defining the Loss-Function and Optimizer ------------------ 
+    criterion = nn.CrossEntropyLoss() 
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+
+    # -------------- Evaluation Dataframes -------------------------------------
+    meta_df_data = {
+        "Category" : ["Epochs", "Optimizer", "Criteroin", "Batch Size", "Learning Rate", "Shuffle", "Pin Memory", "Number Workers"],
+        "Value" : [num_epochs, str(optimizer).replace("\n", ""), str(criterion), batch_size, learning_rate, shuffle, pin_memory, num_workers]
+        }
+    columns=["epoch", "training_loss", "validation_loss", "training_time"]
+    training_df_data = {column : [] for column in columns}
+
+
+    # -------------- Trandformation for the Datasets
+    transform = transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5)),
-            ]
-        )
+            ])
 
+
+    # -------------- Getting the Training and Validation Datasets --------------
     train_set = MSADataset(
         root_dir=os.path.join(dataset_folder, dataset_name),
         annotation_file=os.path.join(dataset_folder, dataset_name, dataset_csv_file),
         transform=transform
         )
     
-    #validation_set = MSADataset(
-    #    root_dir=os.path.join(dataset_folder, validationset_name),
-    #    annotation_file=os.path.join(dataset_folder, validationset_name, dataset_csv_file),
-    #    transform=transform
-    #)
+    if validationset_name != "":
+        validation_set = MSADataset(
+            root_dir=os.path.join(dataset_folder, validationset_name),
+            annotation_file=os.path.join(dataset_folder, validationset_name, dataset_csv_file),
+            transform=transform
+        )
+    else:
+        train_set_size = int(train_set.__len__() * (1-validation_split))
+        validation_set_size = train_set.__len__() - train_set_size
+        train_set, validation_set = torch.utils.data.random_split(train_set,[train_set_size, validation_set_size])
+    meta_df_data["Category"].append("Number Training Samples")
+    meta_df_data["Value"].append(train_set.__len__())
+    meta_df_data["Category"].append("Number Validation Samples")
+    meta_df_data["Value"].append(validation_set.__len__())
 
-    train_set, validation_set = torch.utils.data.random_split(train_set,[209472, 69824])
-    train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
-    validation_loader = DataLoader(dataset=validation_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers, pin_memory=pin_memory)
+    # -------------- Creating Dataloaders --------------------------------------
+    train_loader = DataLoader(
+        dataset=train_set, 
+        shuffle=shuffle, 
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory
+        )
+    validation_loader = DataLoader(
+        dataset=validation_set, 
+        shuffle=shuffle, 
+        batch_size=batch_size,
+        num_workers=num_workers, 
+        pin_memory=pin_memory
+        )
 
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss() 
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    # -------------- Starting the Training Process -----------------------------
     start_time = time.time()
     print("Starting the training process.")
 
-    # Train Network
     for epoch in range(num_epochs):
         losses = []
+        val_losses = []
         
         epoch_start_time = time.time()
-        for batch_idx, (data, targets) in enumerate(train_loader):
-            # Get data to cuda if possible
+        for ((data, targets), (val_data, val_targets)) in zip(train_loader, validation_loader):
+            # get data to cuda if possible
             data = data.to(device=device)
             targets = targets.to(device=device)
+
+            val_data = val_data.to(device=device)
+            val_targets = val_targets.to(device=device)
+            
+            # zero the parameter gradients
+            optimizer.zero_grad()
             
             # forward
             scores = model(data)
             loss = criterion(scores, targets)
-            
             losses.append(loss.item())
+
+            # test the validation data for further analysis
+            val_scores = model(val_data)
+            val_loss = criterion(scores, targets)
+            val_losses.append(val_loss.item())
             
             # backward
-            optimizer.zero_grad()
             loss.backward()
             
             # gradient descent or adam step
@@ -87,6 +142,15 @@ if __name__ == "__main__":
         
         epoch_end_time = time.time()
         epoch_duration = epoch_end_time - epoch_start_time
+        loss_at_epoch = sum(losses)/len(losses)
+        val_loss_at_epoch = sum(val_losses)/len(val_losses)
+
+        # Add new entries for the training dataframe
+        training_df_data["epoch"].append(epoch)
+        training_df_data["training_loss"].append(loss_at_epoch)
+        training_df_data["validation_loss"].append(val_loss_at_epoch)
+        training_df_data["training_time"].append(epoch_duration)
+
         print(f'Cost at epoch {epoch} is {sum(losses)/len(losses)}. Training the epoch took {epoch_duration} seconds.')
 
         if (epoch+1) % 20 == 0:
@@ -95,6 +159,8 @@ if __name__ == "__main__":
 
     end_time = time.time()
     duration = end_time - start_time
+    meta_df_data["Category"].append("Training Duration")
+    meta_df_data["Value"].append(duration)
 
     print(f"Finished training. The process took {duration} seconds.")    
 
@@ -104,7 +170,15 @@ if __name__ == "__main__":
     print("Checking accuracy on Test Set")
     check_accuracy_on_classes(validation_loader, model, device)
 
+    # Create the output directory if it does not exist yet
     if not os.path.exists(model_out_dir):
         os.makedirs(model_out_dir)
 
+    # Save models state dict
     torch.save(model.state_dict(), os.path.join(model_out_dir, model_name))
+
+    # Save the csv files with the training evaluation and meta data 
+    training_df = pd.DataFrame(training_df_data)
+    meta_df = pd.DataFrame(meta_df_data)
+    training_df.to_csv(os.path.join(model_out_dir, "training_data.csv"), index = False, header=True)
+    meta_df.to_csv(os.path.join(model_out_dir, "meta_data.csv"), index = False, header=True)
