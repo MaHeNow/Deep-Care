2c580cc0218ab5d0e44b915db075570fdaaefcef
Mario Nowak <mnowak@students.uni-mainz.de>
2021-03-16 17:34:25 +0100

    bugfix and changes

--- data_generation.py
+++ data_generation.py
@@ -1,5 +1,5 @@
 import os
-
+from glob import glob
 from deepcare.data import generate_examples
 
 if __name__ == "__main__":
@@ -7,15 +7,20 @@ if __name__ == "__main__":
     msa_file_paths = glob(os.path.join(msa_data_path, "part_*"))
 
     fastq_file_path = "/share/errorcorrection/datasets/artmiseqv3humanchr14"
-    fastq_file_name = "humanchr1430cov_3_errFree.fq"
+    fastq_file_name = "humanchr1430cov_3.fq"
+    ref_fastq_file_name = "humanchr1430cov_3_errFree.fq"
+    
     fastq_file = os.path.join(fastq_file_path, fastq_file_name)
+    ref_fastq_file = os.path.join(fastq_file_path, ref_fastq_file_name)
 
-    folder_name = f"datasets/w451_h221/humanchr1430cov_3"
+    folder_name = f"datasets/test/w451_h221/humanchr1430cov_3"
 
     generate_examples(
         msa_file_paths=msa_file_paths,
-        ref_fastq_file_path=fastq_file,
+        ref_fastq_file_path=ref_fastq_file,
+        fastq_file_path=fastq_file,
         image_height= 221,
         image_width=451,
-        out_dir=folder_name
+        out_dir=folder_name,
+        workers=1
     )
