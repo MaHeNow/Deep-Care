475ea1a9bde8573570ad176d75399015570e83b6
Mario Nowak <mnowak@students.uni-mainz.de>
2021-03-16 14:53:44 +0100

    changes

--- parallel_data_generation.py
+++ parallel_data_generation.py
@@ -1,30 +1,21 @@
 import os
 
-from deepcare.data import generate_center_base_train_images_parallel
+from deepcare.data import generate_examples
 
 if __name__ == "__main__":
+    msa_data_path = "/home/mnowak/care/artmiseqv3humanchr1430covMSA_3"
+    msa_file_paths = glob(os.path.join(msa_data_path, "part_*"))
 
-    parts = [i for i in range(32, 36)]
+    fastq_file_path = "/share/errorcorrection/datasets/artmiseqv3humanchr14"
+    fastq_file_name = "humanchr1430cov_3_errFree.fq"
+    fastq_file = os.path.join(fastq_file_path, fastq_file_name)
 
-    for part in parts:
-        msa_data_path = "/home/mnowak/care/artmiseqv3humanchr1430covMSA"
-        msa_file_name = f"part__{part}"
-        msa_file = os.path.join(msa_data_path, msa_file_name)
+    folder_name = f"datasets/w451_h221/humanchr1430cov_3"
 
-        fastq_file_path = "/share/errorcorrection/datasets/artmiseqv3humanchr14"
-        fastq_file_name = "humanchr1430cov_errFree.fq.gz"
-        fastq_file = os.path.join(fastq_file_path, fastq_file_name)
-
-        folder_name = f"datasets/w51_h100/balance_test_4_artmiseqv3humanchr1430covMSA/part_{part}"
-
-        generate_center_base_train_images_parallel(
-            msa_file_path=msa_file,
-            ref_fastq_file_path=fastq_file,
-            image_height= 100,
-            image_width=51,
-            out_dir=folder_name,
-            max_num_examples=None,
-            workers=5,
-            human_readable=True,
-            verbose=True
-        )
+    generate_examples(
+        msa_file_paths=msa_file_paths,
+        ref_fastq_file_path=fastq_file,
+        image_height= 221,
+        image_width=451,
+        out_dir=folder_name
+    )
